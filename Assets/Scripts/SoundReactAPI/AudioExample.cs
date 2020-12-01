fec5d9b0d9686e55d238dc8632c73acbbfb8cefd
Rinkashiki <55148458+Rinkashiki@users.noreply.github.com>
2020-12-01 14:09:58 +0100

    Más funciones con audio

--- Assets/Scripts/SoundReactAPI/AudioExample.cs
+++ Assets/Scripts/SoundReactAPI/AudioExample.cs
@@ -10,35 +10,63 @@ public class AudioExample : MonoBehaviour
     public float startBrightness, brightnessMultiplier;
     public AudioInput audioInput;
 
+    public GameObject[] cubes;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        AudioScale();
-        AudioRotate();
-        AudioBright();
+        /*
+        AmplitudeScale();
+        AmplitudeRotate();
+        AmplitudeBright();
+        */
+        for(int i = 0; i < cubes.Length; i++)
+        {
+            BandScale(cubes[i], i, Vector3.up, scaleMultiplier, startScale);
+            BandBright(cubes[i], i, brightnessMultiplier);
+        };
     }
 
-    private void AudioScale()
+    private void AmplitudeScale(GameObject go, Vector3 axis, float scaleFactor, float startScale)
     {
-        transform.localScale = new Vector3((audioInput.GetAmplitudeBuffer() * scaleMultiplier) + startScale, (audioInput.GetAmplitudeBuffer() * scaleMultiplier)
-                                          + startScale, (audioInput.GetAmplitudeBuffer() * scaleMultiplier) + startScale);
+        go.transform.localScale = new Vector3(((audioInput.GetAmplitudeBuffer() * scaleFactor) * axis.x) + startScale, 
+                                              ((audioInput.GetAmplitudeBuffer() * scaleFactor) * axis.y) + startScale,
+                                              ((audioInput.GetAmplitudeBuffer() * scaleFactor) * axis.z) + startScale);
     }
 
-    private void AudioRotate()
+    private void AmplitudeRotate(GameObject go, Vector3 axis, float rotFactor)
     {
-        transform.Rotate(Vector3.up, audioInput.GetAmplitudeBuffer() * rotMultiplier);
+        go.transform.Rotate(axis, audioInput.GetAmplitudeBuffer() * rotFactor);
     }
 
-    private void AudioBright()
+    private void AmplitudeBright(GameObject go, float brightFactor)
     {
-        float colorValue = startBrightness + audioInput.GetAmplitudeBuffer() * brightnessMultiplier;
+        float colorValue = startBrightness + audioInput.GetAmplitudeBuffer() * brightFactor;
         Color color = new Color(colorValue, colorValue, colorValue);
-        GetComponent<MeshRenderer>().material.color = color;
+        go.GetComponent<MeshRenderer>().material.color = color;
+    }
+
+    private void BandScale(GameObject go, int band, Vector3 axis, float scaleFactor, float startScale)
+    {
+        go.transform.localScale = new Vector3(((audioInput.GetBandBuffer(band) * scaleFactor) * axis.x) + startScale,
+                                               ((audioInput.GetBandBuffer(band) * scaleFactor) * axis.y) + startScale,
+                                               ((audioInput.GetBandBuffer(band) * scaleFactor) * axis.z) + startScale);
+    }
+
+    private void BandRotate(GameObject go, int band, Vector3 axis, float rotFactor)
+    {
+        go.transform.Rotate(axis, audioInput.GetBandBuffer(band) * rotFactor);
+    }
+
+    private void BandBright(GameObject go, int band, float brightFactor)
+    {
+        float colorValue = startBrightness + audioInput.GetBandBuffer(band) * brightFactor;
+        Color color = new Color(colorValue, colorValue, colorValue);
+        go.GetComponent<MeshRenderer>().material.color = color;
     }
 }
