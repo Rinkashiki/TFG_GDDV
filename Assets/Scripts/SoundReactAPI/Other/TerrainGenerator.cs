ee0ab626de917e56ac9ce233642d99557d20bc0a
Rinkashiki <55148458+Rinkashiki@users.noreply.github.com>
2020-12-06 12:37:30 +0100

    Terrain generator

---
+++ Assets/Scripts/SoundReactAPI/Other/TerrainGenerator.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class TerrainGenerator : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    private int xSize = 20;
+    private int zSize = 20;
+
+    public AudioInput audioInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateTerrain();
+        //UpdateTerrain();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateTerrain();
+    }
+
+    private void CreateTerrain()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                //float y = Mathf.PerlinNoise(x * 0.2f, z * 0.2f) * 2;
+                vertices[i] = new Vector3(x, 0, z);
+                i++;
+            } 
+        }
+
+        triangles = new int[xSize * zSize * 6];
+
+        int vert = 0;
+        int tris = 0;
+
+        for (int z = 0; z < zSize; z++)
+        {
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
+
+                vert++;
+                tris += 6;
+            }
+            vert++;
+        }
+    }
+
+    private void UpdateTerrain()
+    {
+        //mesh.Clear();
+        for (int i = 0, z = 0; z <= zSize; z++)
+        {
+            for (int x = 0; x <= xSize; x++)
+            {
+                vertices[i].y = audioInput.GetAmplitudeBuffer() * Mathf.PerlinNoise(x * 0.2f, z * 0.2f) * 3;
+                i++;
+            }
+        }
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+}
